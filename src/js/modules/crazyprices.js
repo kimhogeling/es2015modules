0a31901edb55275a2bea3d775bb14db7fe95c7cd
Kim Hogeling <kim.hogeling@s24.com>
2016-05-05 05:38:48 +0200

    remove temp dead code

--- src/js/modules/crazyprices.js
+++ src/js/modules/crazyprices.js
@@ -22,14 +22,6 @@ const add = (a, b) => a + b
 
 // should be clear as it is almost simple english
 // the dividing by 100 is needed to convert cents back to normal price
-const extractPricesAndSumB = (selector) =>
-    Array.prototype.map.call(qSA(selector), extractPrice).map(convertToCents).reduce(add) / 100
-
-const extractPricesAndSumC = (selector) => {
-    const nodesAsArray = Array.prototype.map.call(qSA(selector), (a) => a)
-    return nodesAsArray.map(extractPrice).map(convertToCents).reduce(add) / 100
-}
-
 const extractPricesAndSum = (selector) =>
     qSA(selector).map(extractPrice).map(convertToCents).reduce(add) / 100
 
